2400dcd5b7225baa233a90a251ccb906dbe85627
Mateusz Wójcikowski <yankes90@gmail.com>
2023-11-03 05:45:24 +0100

    starter

---
+++ meal-app/src/app/meal-search/meal-search.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MealService } from '../meal.service';
+
+@Component({
+  selector: 'app-your-component',
+  template: `
+    <h1>Meal Details</h1>
+    <button (click)="searchMeal('Arrabiata')">Search Arrabiata</button>
+    <div *ngIf="meal">
+      <h2>{{ meal.strMeal }}</h2>
+      <p>{{ meal.strInstructions }}</p>
+    </div>
+  `,
+})
+export class MealSearch {
+  meal: any;
+
+  constructor(private mealService: MealService) {}
+
+  searchMeal(name: string) {
+    this.mealService.getMealsByName(name).subscribe((data) => {
+      this.meal = data.meals[0];
+    });
+  }
+}
